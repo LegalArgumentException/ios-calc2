8e45358210d1a5a71b12163b9dbd9dd4ae2c78f9
Tanner Garrett <tanner@garrett.org>
2016-04-28 17:23:18 -0700

    First commit of stuff

---
+++ simple-calc/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by iGuest on 4/28/16.
+//  Copyright © 2016 Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func Up(sender: UIButton) {
+        NSLog("Button Pressed!")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
